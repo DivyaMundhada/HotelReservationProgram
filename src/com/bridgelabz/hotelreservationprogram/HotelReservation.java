b9a57e8ff1175b26137b5c844bd2f58654a1723c
DivyaMundhada <divyamundada9@gmail.com>
2021-07-26 20:44:42 +0530

    Added Details of Hotel With Name and Rate for Regular Customers.

--- src/com/bridgelabz/hotelreservationprogram/HotelReservation.java
+++ src/com/bridgelabz/hotelreservationprogram/HotelReservation.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.hotelreservationprogram;
 
+import java.util.ArrayList;
+
 public class HotelReservation {
+
+	public void addHotelDetails() {
+		ArrayList<HotelDetails> hotelList = new ArrayList<>();
+		hotelList.add(new HotelDetails("Lakewood", 110));
+		hotelList.add(new HotelDetails("Bridgewood", 160));
+		hotelList.add(new HotelDetails("Ridgewood", 220));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation Program.");
+		HotelReservation hotel = new HotelReservation();
+		hotel.addHotelDetails();
+
 	}
 }
